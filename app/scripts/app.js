4b78288a40ba14c0ff8e43e20fb9eb51a8ec827b
Kasey Markham <kaseycm@gmail.com>
2016-12-04 13:51:55 -0500

    Fixed templates not displaying

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,31 @@
-(function() {
-    function config($stateProvider, $locationProvider) {
-        $locationProvider
-            .html5Mode({
-                enabled: true;
-                requireBase: false;
-            });
+ (function() {
+     function config($stateProvider, $locationProvider) {
+     $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
+    
+     $stateProvider
+         .state('landing', {
+             url: '/',
+             templateUrl: '/templates/landing.html'
+         })
         
-        $stateProvider
-            .state('landing', {
-                url: '/',
-                templateUrl: '/templates/landing.html'
-            })
-            .state('album', {
-                url: '/album',
-                templateUrl: '/templates/album.html'
-            })
-            .state('collection', {
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         
+         })
+            
+         .state('collection', {
                 url: '/collection', 
                 templateUrl: '/templates/collection.html'
-            });
-    }
-    angular
+
+         });
+     }
+ 
+     angular
          .module('blocJams', ['ui.router'])
          .config(config);
  })();
