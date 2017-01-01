f3f1e6f24d0ae66c63d5fc7d43075f2e983c1bf8
Kasey Markham <kaseycm@gmail.com>
2017-01-01 14:36:40 -0500

    Checkpoint 6 work but not working

--- dist/scripts/controllers/CollectionCtrl.js
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -1,12 +1,12 @@
- (function() {
-     function CollectionCtrl() {
+(function() {
+    function CollectionCtrl(Fixtures) {
         this.albums = [];
         for (var i=0; i < 12; i++) {
             this.albums.push(angular.copy(albumPicasso));
         }
-     }
+    }
  
-     angular
-         .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
- })();
+    angular
+        .module('blocJams')
+        .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
+})();
